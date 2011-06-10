66186b547eac1a57d9c96bf3e8f3659072fc1398
Justin Searls <searls@gmail.com>
2011-06-10 10:49:08 -0400

    got basic styling done for /, #, and -

--- public/js/doing-it.js
+++ public/js/doing-it.js
@@ -1,6 +1,13 @@
 (function($,_) {
-	window.DoingIt = function() {
-		var self = {};
+	window.DoingIt = function($editor) {
+		var self = {
+			types: {
+				'-': 'pending',
+				'/': 'finished',
+				'#': 'blocked'
+			}
+		};
+		
 		
 		self.init = function() {
 			restoreFromLocalStorage();
@@ -9,17 +16,29 @@
 		
 		self.doIt = function() {
 			wrapAnyRawTextInADiv();
+			decorateLines();
 			saveToLocalStorage();
 		};
 		
 		var wrapAnyRawTextInADiv = function() {
-			//Safari wraps each newline in a div, but sometimes misses the first one
-			$('#editor').contents().filter(function(){ return this.nodeType == 3; }).wrap('<div></div>');
+			//Safari wraps each newline in a div, except it always leaves the first one naked
+			$editor.contents().filter(function(){ return this.nodeType == 3; }).wrap('<div></div>');
 		}
+		
+		var decorateLines = function() {
+			rows().each(function(i,el) {
+				var $row = $(el);
+				$row.removeClass(); //Safari will otherwise keep appending the classes of the previous ones
+				var type = self.types[$row.text().charAt(0)];
+				if(type) {
+					$row.addClass(type).addClass('task');
+				}
+			});
+		};
 		
 		var saveToLocalStorage = function() {
 			var items = [];
-			$('#editor > div').each(function(i,item) {
+			rows().each(function(i,item) {
 				items.push($(item).text());
 			});
 			localStorage['items'] = JSON.stringify(items);
@@ -30,18 +49,22 @@
 				var items = JSON.parse(localStorage['items']);
 				_(items).each(function(item) {
 					item = !item ? "<br/>" : item;
-					$('#editor').append('<div>'+item+'</div>');
+					$editor.append('<div>'+item+'</div>');
 				});
 			} else {
-				$('#editor').append('<div><br/></div>');
+				$editor.append('<div><br/></div>');
 			}
 		};
+		
+		var rows = function() {
+			return $editor.find('> div');
+		}
 		
 		return self;
 	};
 	
 	$(function() {
-		var doingIt = DoingIt();
+		var doingIt = DoingIt($('#editor'));
 		doingIt.init();
 		$('#editor').live('keydown',doingIt.doIt);
 	});
